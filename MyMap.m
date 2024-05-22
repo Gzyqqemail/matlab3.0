4c8549a68f5b9174438603080cdb24295324abe2
高哲元 <2020300354@mail.nwpu.edu.cn>
2024-05-22 13:32:56 +0800

    将画图、控制两个功能放在Mymap,control两个文件里

--- MyMap.m
+++ MyMap.m
@@ -1,5 +1,9 @@
 function out = MyMap(UavTeam,Obstacle,Highway)
-
+global time
+persistent gfigure;
+if isempty(gfigure)
+    gfigure = 1;
+end
 M = UavTeam.AvailableNumMax;
 % 2D map
 % for k = 1: M
@@ -19,7 +23,7 @@ line21 = [Highway(1).ph1(1)  Highway(1).ph2(1)-4500]';
 line22 = [Highway(1).ph1(2)-Highway(1).rh  Highway(1).ph2(2)-Highway(1).rh]';
 plot(line21,line22);
 hold on
-
+%计算每个无人机的位置和滤波位置，并画出无人机的三个区域
 for k = 1: M
     o1 = [UavTeam.Uav(k).CurrentPos(1) UavTeam.Uav(k).CurrentPos(2)]';
     o2 = [UavTeam.Uav(k).CurrentPos(1)+1/UavTeam.gain*UavTeam.Uav(k).Velocity(1) UavTeam.Uav(k).CurrentPos(2)+1/UavTeam.gain*UavTeam.Uav(k).Velocity(2)]';
@@ -29,5 +33,76 @@ axis([-600 600 -600 600])
 grid on
 xlabel('x')
 ylabel('y')
-
+% 在图的特定位置显示仿真时间
+xPosition = -600; % x 轴位置
+yPosition = 500; % y 轴位置
+text(xPosition, yPosition, ['仿真时间: ' num2str(time)], 'FontSize', 12, 'Color', 'red');
+title(['第' num2str(gfigure) '张图'] );
+gfigure = gfigure+1;
 out = 0;
+end
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function out=mydrawcolorball(o1,o2,k)
+%     if (k==1) 
+%         plot(o1(1),o1(2),'.','MarkerSize',15,'MarkerEdgeColor','y','MarkerFaceColor', 'y');
+%         radiusplot(o1,o2,k)
+% %         hold on
+%     elseif (k==2)
+%        plot(o1(1),o1(2),'.','MarkerSize',15,'MarkerEdgeColor','m','MarkerFaceColor', 'm');
+%        radiusplot(o1,o2,k)
+% %         hold on
+%     elseif (k==3)
+%        plot(o1(1),o1(2),'.','MarkerSize',15,'MarkerEdgeColor','c','MarkerFaceColor', 'c');
+%        radiusplot(o1,o2,k)
+% %         hold on
+%     elseif (k==4)
+%         plot(o1(1),o1(2),'.','MarkerSize',15,'MarkerEdgeColor','r','MarkerFaceColor','r');
+%         radiusplot(o1,o2,k)
+% %         hold on
+%     elseif (k==5)
+%         plot(o1(1),o1(2),'.','MarkerSize',15,'MarkerEdgeColor','g','MarkerFaceColor', 'g');
+%         radiusplot(o1,o2,k)
+% %         hold on       
+%     elseif k==6
+%         plot(o1(1),o1(2),'.','MarkerSize',15,'MarkerEdgeColor','b','MarkerFaceColor', 'b');
+%         radiusplot(o1,o2,k)
+% %         hold on  
+%     else
+%         plot(o1(1),o1(2),'.','MarkerSize',15,'MarkerEdgeColor','k','MarkerFaceColor', 'k');
+        radiusplot(o1,o2,k)
+%         hold on        
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function out=radiusplot(o1,o2,k)
+global rm rs ra rd
+%         mycircle(o1,rm)
+mycircle_y(o1,rd)
+mycircle_g(o2,ra)
+mycircle_b(o2,rs)
+plot(o1(1),o1(2),'k-o','MarkerFaceColor','k','MarkerSize',4)
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function out = mycircle_b(o,r)
+ alpha = 0:pi/20:2*pi;    
+ x = o(1) +  r*cos(alpha); 
+ y = o(2) +  r*sin(alpha); 
+ plot(x,y,'b-') 
+end
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ function out = mycircle_g(o,r)
+ alpha = 0:pi/20:2*pi;    
+ x = o(1) +  r*cos(alpha); 
+ y = o(2) +  r*sin(alpha); 
+ plot(x,y,'g-') 
+ end
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ function out = mycircle_y(o,r)
+ alpha = 0:pi/20:2*pi;    
+ x = o(1) +  r*cos(alpha); 
+ y = o(2) +  r*sin(alpha); 
+ plot(x,y,'y-') 
+ end
--- mycontrol.m
+++ mycontrol.m
@@ -16,14 +16,14 @@ pt1  = Highway(1).ph1;
 pt2  = Highway(1).ph2;
  rt = Highway(1).rh;
 pt3 = Highway(1).ph2 + [0 rt]';
- ksi  = Pcur + 1/UavTeam.gain*Vcur;
+ ksi  = Pcur + 1/UavTeam.gain*Vcur;%滤波位置
  rb = 0;
- rsr= 10000;
+ rsr= 10000;%另两个虚拟管道的半径
  rrt= 10000;
  
  if UavTeam.Uav(i).State == 1  % Left Standby Area
     psr1 = [pt2(1) rt+rsr]';
-    psr2 = [-rsr rt+rsr]';
+    psr2 = [-rsr rt+rsr]';%这里是不是有问题，应该是[-rb rt+rsr]'，管道终点线在-rsr处？
     psr3 = [-rsr rt]';
        u = mytunnel(i,psr1,psr2,psr3,rsr);
     
@@ -34,8 +34,8 @@ pt3 = Highway(1).ph2 + [0 rt]';
        u = mytunnel(i,psr1p,psr2p,psr3p,rsr); 
        
  elseif  UavTeam.Uav(i).State == 3 % Left Ready Area
-   prt1 = [-rrt rt+rrt]';
-   prt2 = [-rrt 0]';
+   prt1 = [-rrt rt+rrt]';%把准备区的长度也设为rrt了，长度等于一半的宽度
+   prt2 = [-rrt 0]';%把管道终点线设为纵坐标为0了
    prt3 = [-2*rrt    0]';
        u = mytunnel(i,prt1,prt2,prt3,rrt); 
        
@@ -48,4 +48,114 @@ pt3 = Highway(1).ph2 + [0 rt]';
  else% Tunnel and Tunnel Extension
        u = mytunnel(i,pt1,pt2,pt3,rt); 
  end
-    
+end
+
+
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ function u = mytunnel(i,p1,p2,p3,rh)
+global UavTeam
+
+Pcur  =  UavTeam.Uav(i).CurrentPos;
+Vcur  =  UavTeam.Uav(i).Velocity;
+Pdes  =  UavTeam.Uav(i).Waypoint(:,UavTeam.Uav(i).CurrentTaskNum);
+rs = UavTeam.Uav(i).rs;
+ra = UavTeam.Uav(i).ra;
+vmax = UavTeam.Uav(i).vmax;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+At12 = eye(2) - (p1-p2)*(p1-p2)'/(norm(p1-p2)*norm(p1-p2));
+At23 = eye(2) - (p2-p3)*(p2-p3)'/(norm(p2-p3)*norm(p2-p3));
+
+% Velocity to line
+k1 = 1;
+el = Pcur + 1/UavTeam.gain*Vcur - p2;
+Vl = -mysat(k1*At23*el,vmax);
+
+% Velocity away other multicoptee
+Vm = [0;0];
+for l = 1:UavTeam.AvailableNumMax
+    if i~=l && norm(Pcur)~=0
+        k2 = 1; e = 0.000001;
+        ksimil = (Pcur - UavTeam.Uav(l).CurrentPos) + 1/UavTeam.gain*(Vcur - UavTeam.Uav(l).Velocity);
+        temp = (1+e)*norm(ksimil) - (2*rs)*mys(norm(ksimil)/(2*rs),e);
+        bil = k2*dmysigma(norm(ksimil),2*rs,rs+ra)/temp ...
+            - k2*mysigma(norm(ksimil),2*rs,rs+ra)*((1+e)-dmys(norm(ksimil)/(2*rs),e))/(temp^2);
+        Vm = Vm - bil*(ksimil/norm(ksimil));
+    end
+end
+
+% Velocity within the tunnel
+Vt = [0;0];
+k3 = 1; e = 0.001;
+ksihi = At12*((Pcur-p2) + 1/UavTeam.gain*Vcur);
+temp1  = (rh-rs)/(norm(ksihi)+e);
+temp2  = (rh-rs) - norm(ksihi)*mys(temp1,e);
+%% wrong 负号
+ci = -k3*dmysigma(rh-norm(ksihi),rs,rs+ra)/temp2 ...
+    - k3*mysigma(rh-norm(ksihi),rs,rs+ra)*(-mys(temp1,e)+norm(ksihi)*dmys(temp1,e)*(rh-rs)/(norm(ksihi)+e)/(norm(ksihi)+e))/(temp2^2);
+Vt = Vt - ci*(ksihi/norm(ksihi));
+
+% Sum of all velocities
+u = mysat(Vl+Vm+Vt,vmax);
+ end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function u=mysat(x,a)
+% ensure the direction to be the same
+    if norm(x)>a
+       u =a*x/norm(x);
+    else
+       u =x ;
+    end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function u=mys(x,rs)
+
+x2 =  1 + 1/tan(67.5/180*pi)*rs;
+x1 = x2 - sin(45/180*pi)*rs;
+if x<=x1
+    u = x;
+elseif x1<=x  & x<=x2
+    u = (1-rs)+sqrt(rs^2-(x-x2)^2);
+else
+    u = 1;
+end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function u=mysigma(x,d1,d2)
+
+if x<=d1
+    u = 1;
+elseif d1<=x  & x<=d2
+    A = -2/((d1-d2)^3); B = 3*(d1+d2)/((d1-d2)^3); C = -6*d1*d2/((d1-d2)^3); D = d2^2*(3*d1-d2)/((d1-d2)^3);
+    u = A*x^3 + B*x^2 + C*x + D;
+else
+    u = 0;
+end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function u=dmys(x,rs)
+
+x2 =  1 + 1/tan(67.5/180*pi)*rs;
+x1 = x2 - sin(45/180*pi)*rs;
+if x<=x1
+    u = 1;
+elseif x1<=x  & x<=x2
+    u = (x2-x)/sqrt(rs^2-(x-x2)^2)/2;
+else
+    u = 0;
+end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function u=dmysigma(x,d1,d2)
+
+if x<=d1
+    u = 0;
+elseif d1<=x  & x<=d2
+    A = -2/((d1-d2)^3); B = 3*(d1+d2)/((d1-d2)^3); C = -6*d1*d2/((d1-d2)^3);
+    u = 3*A*x^2 + 2*B*x + C;
+else
+    u = 0;
+end
+end
+  
